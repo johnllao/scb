df0e8fe8b048e728a07c50ebf57649499ee7656c
John Lao <john.lao@gmail.com>
2016-11-30 11:30:32 +0800

    added help servlet

---
+++ basictomcat/src/main/java/basictomcat/servlets/HelpServlet.java
@@ -0,0 +1,28 @@
+package basictomcat.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name="HelpServlet", 
+            urlPatterns="/help")
+public class HelpServlet extends HttpServlet {
+
+	private static final long serialVersionUID = -7191151071503980878L;
+
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+		
+		final String[] name = req.getParameterValues("name");
+		req.getServletContext().setAttribute("name", name == null || name[0] == null ? "none" : name[0]);
+		
+		final RequestDispatcher dispatcher = req.getRequestDispatcher("help.jsp");
+		dispatcher.forward(req, res);
+		
+	}
+}
---
+++ basictomcat/src/main/webapp/help.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1" pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+	<title>Help</title>
+</head>
+<body>
+	<div>
+		Welcome! <%= this.getServletContext().getAttribute("name") %>
+	</div>
+</body>
+</html>
