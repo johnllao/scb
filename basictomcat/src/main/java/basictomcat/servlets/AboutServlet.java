445494149a8a9645e6acb192e9b9f59231efd2c3
John Lao <john.lao@gmail.com>
2016-11-30 11:06:12 +0800

    added about servlet

---
+++ basictomcat/src/main/java/basictomcat/servlets/AboutServlet.java
@@ -0,0 +1,31 @@
+package basictomcat.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name="AboutServlet", 
+            urlPatterns="/about")
+public class AboutServlet extends HttpServlet{
+	
+	private static final long serialVersionUID = 2733502798402137000L;
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		
+		final String[] author = req.getParameterValues("author");
+			
+		res.setHeader("Content-type", "text/plain");
+		
+		final PrintWriter writer = res.getWriter();
+		writer.println("Servlet Demo");
+		writer.println("version 1.0.0");
+		writer.printf("author: %s\n", author == null || author[0] == null ? "none" : author[0]);
+		
+		writer.flush();
+	}
+
+}
