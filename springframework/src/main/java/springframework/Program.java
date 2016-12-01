e8a50c1e2f216d95123531af7e9be2c0aef0f8ff
John Lao <john.lao@gmail.com>
2016-12-01 11:45:20 +0800

    change to filesys

--- springframework/src/main/java/springframework/Program.java
+++ springframework/src/main/java/springframework/Program.java
@@ -1,14 +1,14 @@
 package springframework;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class Program {
 
 	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		final ApplicationContext context =
-			    new ClassPathXmlApplicationContext("dependencies.xml");
+			    new FileSystemXmlApplicationContext("dependencies.xml");
 		final MessagePrinter printer = context.getBean("messagePrinter", MessagePrinter.class);
 		printer.printMessage();
 
