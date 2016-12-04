10d945a64818628d99cc8e5d17ecbf3043cc3035
John Lao <john.lao@gmail.com>
2016-12-04 12:01:19 +0800

    changed the response type to json

--- springboot/src/main/java/springboot/api/AppInfoController.java
+++ springboot/src/main/java/springboot/api/AppInfoController.java
@@ -1,16 +1,22 @@
 package springboot.api;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api")
 public class AppInfoController {
 
-	@RequestMapping("version")
-    public String version() {
-        return "1.0.0";
+	@RequestMapping(path="version", produces=MediaType.APPLICATION_JSON_VALUE)
+    public Map<String, Object> version() {
+		final Map<String, Object> result = new HashMap<String, Object>();
+		result.put("version", "1.0.0");
+        return result;
     }
 
-	
 }
