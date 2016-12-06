6bfc86da49322314970bbf4dc17998dab2226b86
John Lao <john.lao@gmail.com>
2016-12-06 22:48:30 +0800

    added readme

---
+++ phonebook/README.md
@@ -0,0 +1,3 @@
+## References
+- [Spring Framework](http://docs.spring.io/spring/docs/current/spring-framework-reference/htmlsingle/)
+- [Spring Data JPA](http://docs.spring.io/spring-data/jpa/docs/1.10.5.RELEASE/reference/html/)
--- phonebook/src/main/resources/static/js/app/index.js
+++ phonebook/src/main/resources/static/js/app/index.js
@@ -2,18 +2,9 @@
 	
 	var app = angular.module('app', []);
 	
-	app.controller('indexCtrl', ['$http', '$scope', function(http, model) { 
+	app.controller('controller', ['$http', '$scope', function(http, model) { 
 		
-		http.get('/tomcatjersey/api/products')
-		    .then(function(res) { 
-		    	
-		    	model.products = res.data;
-		    	
-		    }, function(res) { 
-		    	
-		    	alert(res.statusText);
-		    	
-		    });
+		model.message = 'welcome';
 		
 	}]);
 	
